c78e9c2d6528502727889008d7cd0078e6aab6a5
Petiot Thibaut <petiotthib@gmail.com>
2018-04-03 14:37:12 +0200

    dll missing => reimport probuilder, add trail, add slow mo

--- SRC/Assets/Scripts/CameraBehaviour.cs
+++ SRC/Assets/Scripts/CameraBehaviour.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CameraBehaviour : MonoBehaviour {
 
-	public Transform Target;
+	public Transform[] Targets;
 
 	private Transform _trans;
 	private Vector3 _posCamera;
@@ -14,9 +14,21 @@ public class CameraBehaviour : MonoBehaviour {
 		_trans = transform;
 		_posCamera = _trans.position;
 	}
-	void Update ()
+	private void Update ()
 	{
-		_posCamera.Set(Target.position.x, Target.position.y, _posCamera.z);
+		var posX = 0f;
+		var posY = 0f;
+		var count = 0;
+		for (var i = Targets.Length - 1; i >= 0; i--)
+		{
+			if (!Targets[i])
+				continue;
+
+			posX += Targets[i].position.x;
+			posY += Targets[i].position.y;
+			++count;
+		}
+		_posCamera.Set(posX / count, posY / count, _posCamera.z);
 		_trans.position = _posCamera;
 	}
 }
--- SRC/Assets/Scripts/PawnComponent.cs
+++ SRC/Assets/Scripts/PawnComponent.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -13,14 +14,22 @@ public class PawnComponent : MonoBehaviour {
 	public LayerMask LayerWakeable;
 
 	public float ShootForce;
-	public Rigidbody2D TargetShoot;
+	public PawnComponent TargetShoot;
+	public float FireRate;
 
+	public float DurationSlowMo;
+	public AnimationCurve CurveSlowMo;
+	public AnimationCurve CurveScale;
+
+	public Rigidbody2D PawnRigid2D { get; private set; }
 
 	private Transform _trans;
-	private Rigidbody2D _rigid2D;
+	private Transform _graphBody;
 	
 	private Vector2 _deltaMove;
 	private Vector2 _aimDir;
+
+	private float _timerFire;
 
 	private bool _inputJump;
 	private bool _inputFire;
@@ -31,12 +40,17 @@ public class PawnComponent : MonoBehaviour {
 		SpeedMove = 10f;
 		JumpForce = new Vector2(10f, 0f);
 		LeftBorder = new Vector2(0f, 0.5f);
+		FireRate = 1f;
+		DurationSlowMo = 0.5f;
 	}
 
 	private void Awake()
 	{
 		_trans = transform;
-		_rigid2D = GetComponent<Rigidbody2D>();
+		PawnRigid2D = GetComponent<Rigidbody2D>();
+		_graphBody = _trans.Find("Body");
+		if (!_graphBody)
+			_graphBody = _trans;
 	}
 
 
@@ -79,37 +93,114 @@ public class PawnComponent : MonoBehaviour {
 		if (!_isGrounded || !_inputJump)
 			return;
 
-		_rigid2D.AddForce(JumpForce, ForceMode2D.Impulse);
+		PawnRigid2D.AddForce(JumpForce, ForceMode2D.Impulse);
 		
 	}
 
 	private void MoveLogic()
 	{
-		_rigid2D.velocity = _deltaMove + new Vector2(0f, _rigid2D.velocity.y);
+		PawnRigid2D.velocity = _deltaMove + new Vector2(0f, PawnRigid2D.velocity.y);
 		_deltaMove.Set(0f, 0f);
 	}
 
 	private void UpdateIsGrounded()
 	{
-		Vector2 pos = new Vector2(_trans.position.x, _trans.position.y);
+		var pos = new Vector2(_trans.position.x, _trans.position.y);
 		var hitInfo = Physics2D.Raycast(LeftBorder + pos, Vector2.down, 0.2f, LayerWakeable);
 		if (hitInfo.transform != null)
 			_isGrounded = true;
 		else
 			_isGrounded = false;
 	}
+
+	private IEnumerator SlowMoShoot()
+	{
+		for (float t = 0f, perc = 0f; perc < 1f; t += Time.unscaledDeltaTime)
+		{
+			perc = Mathf.Clamp01(t / DurationSlowMo);
+			Time.timeScale = CurveSlowMo.Evaluate(perc);
+			yield return null;
+		}
+	}
 	
 	private void FireLogic()
 	{
-		if (!_inputFire || _aimDir == Vector2.zero)
+		_timerFire += Time.fixedDeltaTime;
+
+		if (!_inputFire || _aimDir == Vector2.zero || _timerFire < 0f)
 			return;
 
+		_timerFire = -FireRate;
 		_inputFire = false;
 
 		//TargetShoot.MovePosition(new Vector2(_trans.position.x, _trans.position.y));
-		TargetShoot.transform.position = _trans.position;
-		TargetShoot.velocity = Vector2.zero;
-		TargetShoot.AddForce(_aimDir * ShootForce, ForceMode2D.Impulse);
 
+		StartCoroutine(FireSlowMo());
+	}
+
+	private IEnumerator ScaleEnum(bool toOne, float duration)
+	{
+		var beg = toOne ? Vector3.zero : Vector3.one;
+		var end = !toOne ? Vector3.zero : Vector3.one;
+		for (float t = 0f, perc = 0f; perc < 1f; t += Time.unscaledDeltaTime)
+		{
+			perc = Mathf.Clamp01(t / duration);
+			_graphBody.localScale = Vector3.Lerp(beg, end, CurveScale.Evaluate(perc));
+			yield return null;
+		}
+	}
+
+	private IEnumerator FireSlowMo()
+	{
+		Debug.Log("Beg slow mo");
+		Time.timeScale = 0f;
+		//Move & scale
+		StartCoroutine(HelperTween.MoveTransformEnum(TargetShoot.transform, TargetShoot.transform.position, _trans.position,
+			0.5f, AnimationCurve.EaseInOut(0f, 0f, 1f, 1f)));
+		yield return HelperTween.ScaleEnum(TargetShoot.transform, TargetShoot.transform.localScale, Vector3.zero, 0.5f, AnimationCurve.EaseInOut(0f, 0f, 1f, 1f));
+
+		TargetShoot.transform.position = _trans.position;
+		TargetShoot.PawnRigid2D.velocity = Vector2.zero;
+
+		TargetShoot.PawnRigid2D.AddForce(_aimDir * ShootForce, ForceMode2D.Impulse);
+		StartCoroutine(HelperTween.ScaleEnum(TargetShoot.transform, TargetShoot.transform.localScale, Vector3.one, 0.3f,
+			AnimationCurve.EaseInOut(0f, 0f, 1f, 1f)));
+		yield return HelperTween.TimeScaleEnum(0f, 1f, 0.3f, AnimationCurve.EaseInOut(0f, 0f, 1f, 1f));
+		//Move & scale and timescale
+		Debug.Log("End slow mo");
+	}
+}
+
+
+public static class HelperTween
+{
+
+	public static IEnumerator ScaleEnum(Transform target, Vector3 beg, Vector3 end, float duration, AnimationCurve curve, bool useUnscaledDeltaTime = true)
+	{
+		return IterateEnum((value) => target.localScale = value, Vector3.Lerp, beg, end, duration, curve, useUnscaledDeltaTime);
+	}
+
+	public static IEnumerator MoveRigid2DEnum(Rigidbody2D target, Vector3 beg, Vector3 end, float duration, AnimationCurve curve, bool useUnscaledDeltaTime = true)
+	{
+		return IterateEnum((value) => target.MovePosition(value), Vector3.Lerp, beg, end, duration, curve, useUnscaledDeltaTime);
+	}
+	public static IEnumerator MoveTransformEnum(Transform target, Vector3 beg, Vector3 end, float duration, AnimationCurve curve, bool useUnscaledDeltaTime = true)
+	{
+		return IterateEnum((value) => target.position = value, Vector3.Lerp, beg, end, duration, curve, useUnscaledDeltaTime);
+	}
+
+	public static IEnumerator TimeScaleEnum(float beg, float end, float duration, AnimationCurve curve, bool useUnscaledDeltaTime = true)
+	{
+		return IterateEnum((value) => Time.timeScale = value, Mathf.Lerp, beg, end, duration, curve, useUnscaledDeltaTime);
+	}
+
+	private static IEnumerator IterateEnum<T>(Action<T> callback, Func<T, T, float, T> lerpFunc, T beg, T end, float duration, AnimationCurve curve, bool useUnscaledDeltaTime)
+	{
+		for (float t = 0f, perc = 0f; perc < 1f; t += useUnscaledDeltaTime ? Time.unscaledDeltaTime : Time.deltaTime)
+		{
+			perc = Mathf.Clamp01(t / duration);
+			callback(lerpFunc(beg, end, curve.Evaluate(perc)));
+			yield return null;
+		}
 	}
 }
